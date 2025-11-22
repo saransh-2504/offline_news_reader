424a00e7da5513fd1e395a19e301da411a1ab4a3
saransh-2504 <saransh2504@gmail.com>
2025-11-23 02:09:23 +0530

    implement gnews api

--- java.js
+++ java.js
@@ -114,3 +114,91 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("Logged out!");
   }
 });
+
+//  NEWS API + SEARCH + CATEGORY
+
+  const API_KEY = "01f34776ac18f47af61d0c44277a190d"; //01f34776ac18f47af61d0c44277a190d
+  const newsContainer = document.querySelector(".news-container");
+  const searchInput = document.getElementById("searchInput");
+  const categoryButtons = document.querySelectorAll(".cat");
+
+  let currentCategory = "general";
+  let searchText = "";
+
+  // debounce
+  let timer;
+  function debounce(fn, delay) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(fn, delay);
+  }
+
+// FETCH NEWS FROM GNEWS
+
+  function loadNews() {
+
+// Build API URL
+    let url = `https://gnews.io/api/v4/search?q=${searchText || "latest"}&topic=${currentCategory}&lang=en&apikey=${API_KEY}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        displayNews(data.articles);
+      })
+      .catch(() => {
+        newsContainer.innerHTML = "<h2>Error loading news.</h2>";
+      });
+  }
+
+  // DISPLAY NEWS IN GRID
+
+  function displayNews(articles) {
+
+    newsContainer.innerHTML = ""; 
+
+    if (!articles || articles.length === 0) {
+      newsContainer.innerHTML = "<h2>No news found.</h2>";
+      return;
+    }
+
+    articles.forEach(article => {
+
+      let card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <img src="${article.image || "https://via.placeholder.com/300"}" />
+        <div class="content">
+          <div class="tag">${currentCategory.toUpperCase()}</div>
+          <div class="title-text">${article.title}</div>
+          <div class="desc">${article.description || "No description available."}</div>
+          <div class="actions">
+            <span>❤️ Save</span>
+            <a href="${article.url}" target="_blank" class="btn">Read More</a>
+          </div>
+        </div>
+      `;
+
+      newsContainer.appendChild(card);
+    });
+  }
+
+// CATEGORY CLICK
+
+  categoryButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+      currentCategory = btn.dataset.category;
+      loadNews();
+    });
+  });
+
+// SEARCH INPUT (Debounced)
+
+  searchInput.addEventListener("input", () => {
+    debounce(() => {
+      searchText = searchInput.value.trim();
+      loadNews();
+    }, 500);
+  });
+
+  loadNews();
